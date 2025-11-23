229fd779d8941a573628c627da9b6003192697f4
shigemasa89 <shigemasa3mishima@gmail.com>
2025-11-23 18:16:02 +0900

    練習1-3の回答を追加

---
+++ FiveValues.java
@@ -0,0 +1,9 @@
+public class FiveValues {
+    public static void main(String[] args) {
+        boolean bool = true;
+        char drive = '駆';
+        final double PIE = 3.14;
+        final long LongPIE = 314159265853979L;
+        String attackMessage = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    }
+}
